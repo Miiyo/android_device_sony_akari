738b18e5fb7b318956f30b2bf4f730d160facea5
ekkusa <dayesofficial@gmail.com>
2023-04-24 16:35:49 +0800

    akari: Import Properties

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,5 +15,9 @@ TARGET_SCREEN_DENSITY := 440
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
+
 # Inherit from the proprietary version
 include vendor/sony/akari/BoardConfigVendor.mk
