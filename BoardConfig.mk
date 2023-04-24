988cea0e6f9989896079c679fe8a04e88cbdc262
ekkusa <dayesofficial@gmail.com>
2023-04-24 15:34:38 +0800

    akari: Initial Skeleton Device Tree

---
+++ BoardConfig.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/sony/akari
+
+# Display
+TARGET_SCREEN_DENSITY := 440
+
+# Inherit from the proprietary version
+include vendor/sony/akari/BoardConfigVendor.mk
