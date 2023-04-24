551961b64d33bf5dde3ac5cc4e609c04d13d297e
ekkusa <dayesofficial@gmail.com>
2023-04-24 16:33:33 +0800

    akari: Import HIDL Manifest

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,8 @@ DEVICE_PATH := device/sony/akari
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
+
 # Inherit from the proprietary version
 include vendor/sony/akari/BoardConfigVendor.mk
